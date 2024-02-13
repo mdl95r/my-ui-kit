61ac94c01439f6f39b3271cca9652e0e9fda8563
Artem Bogomolov <awesomemdl95@gmail.com>
2024-02-13 21:36:55 +0300

    Spinner component

---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,60 @@
+<template>
+  <Icon class="spinner" :name="spinnerIcon" :size="size" :size-em="false" :style="{ animationDuration: `${animationDuration}s` }" />
+</template>
+
+<script>
+import Icon from '../Icon';
+
+  export default {
+    name: 'UiSpinner',
+    components: {
+      Icon,
+    },
+
+    props: {
+      /**
+      * Размер иконки [sm, md, lg]
+      */
+      size: {
+        type: String,
+        default: 'lg'
+      },
+
+      /**
+      * Скорость вращения спиннера, если указано (по умолчанию 2s)
+      */
+      animationDuration: {
+        type: Number,
+        default: null
+      },
+
+      /**
+      * Иконка вращения
+      */
+      spinnerIcon: {
+        type: String,
+        default: 'spinner',
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@keyframes rotate-animation {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  50% {
+    transform: rotate(180deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.spinner {
+  animation: rotate-animation 2s infinite linear
+}
+</style>
---
+++ stories/Spinner.stories.js
@@ -0,0 +1,9 @@
+import Spinner from "../src/components/Spinner";
+
+export default {
+  title: 'Elements/Spinner',
+  component: Spinner,
+  tags: ['autodocs'],
+};
+
+export const Base = {};
