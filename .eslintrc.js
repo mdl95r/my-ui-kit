e15b67f37061e54f5b1e52291141f8b96f20d9f4
Artem Bogomolov <awesomemdl95@gmail.com>
2024-03-15 22:00:43 +0300

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   extends: [
+    'eslint:recommended',
     'plugin:vue/vue3-recommended',
     "prettier",
   ],
-  rules: {}
+  rules: {
+    "vue/multi-word-component-names": 0
+  }
 }
