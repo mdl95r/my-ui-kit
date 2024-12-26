a51433a485df1b54847209c864ed96c59d2eb42a
Artem Bogomolov <awesomemdl95@gmail.com>
2024-12-26 22:16:37 +0300

    удалил storybook-addon-vue-slots

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,6 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-vue-slots',
   ],
   framework: {
     name: '@storybook/vue3-vite',
